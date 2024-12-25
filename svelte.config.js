b5356d9eab6df788a60513b088955226a895ac9b
Ankur Singh <abishek.singh.chauhan@gmail.com>
2024-12-25 16:06:14 +0000

    Add Posts api
    Render posts from posts api.

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,18 @@ import { mdsvex } from 'mdsvex';
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: ['.md'],
+}
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [vitePreprocess(), mdsvex()],
+	extensions: ['.svelte', '.md'],
+	preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
 	kit: {
 		adapter: adapter()
-	},
-	extensions: ['.svelte', '.svx']
-};
+	}
+}
 
 export default config;
